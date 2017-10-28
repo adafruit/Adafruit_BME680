0d1cbaeda3f9a02314eaedc66a9847592ca4d306
ladyada <limor@ladyada.net>
2017-10-28 17:42:01 -0400

    fix odd avr compiling complaint, add SPI hardware/software support

--- Adafruit_BME680.cpp
+++ Adafruit_BME680.cpp
@@ -17,21 +17,32 @@
 #include "Arduino.h"
 #include "Adafruit_BME680.h"
 
-#define DEBUG_I2C
+#define BME680_DEBUG
+
+
+// must be global in order to work with underlying library
+int8_t _BME680_SoftwareSPI_MOSI, _BME680_SoftwareSPI_MISO, _BME680_SoftwareSPI_SCK;
+
 
 /***************************************************************************
- PRIVATE FUNCTIONS
+ PUBLIC FUNCTIONS
  ***************************************************************************/
 
-Adafruit_BME680::Adafruit_BME680(int8_t cspin) 
-  : _cs(cspin), _mosi(-1), _miso(-1), _sck(-1)
+Adafruit_BME680::Adafruit_BME680(int8_t cspin)
+  : _cs(cspin)
 {
+  _BME680_SoftwareSPI_MOSI = -1;
+  _BME680_SoftwareSPI_MISO = -1;
+  _BME680_SoftwareSPI_SCK = -1;
   _filterEnabled = _tempEnabled = _humEnabled = _presEnabled = _gasEnabled = false;
 }
 
 Adafruit_BME680::Adafruit_BME680(int8_t cspin, int8_t mosipin, int8_t misopin, int8_t sckpin)
-  : _cs(cspin), _mosi(mosipin), _miso(misopin), _sck(sckpin)
+  : _cs(cspin)
 {
+  _BME680_SoftwareSPI_MOSI = mosipin;
+  _BME680_SoftwareSPI_MISO = misopin;
+  _BME680_SoftwareSPI_SCK = sckpin;
   _filterEnabled = _tempEnabled = _humEnabled = _presEnabled = _gasEnabled = false;
 }
 
@@ -47,27 +58,33 @@ bool Adafruit_BME680::begin(uint8_t addr) {
     gas_sensor.intf = BME680_I2C_INTF;
     gas_sensor.read = &i2c_read;
     gas_sensor.write = &i2c_write;
-    gas_sensor.delay_ms = delay;
-
-    int8_t rslt = BME680_OK;
-    rslt = bme680_init(&gas_sensor);
-    //Serial.print("Result: "); Serial.println(rslt);
-    if (rslt != BME680_OK) 
-      return false;
   } else {
     digitalWrite(_cs, HIGH);
     pinMode(_cs, OUTPUT);
 
-    if (_sck == -1) {
+    if (_BME680_SoftwareSPI_SCK == -1) {
       // hardware SPI
       SPI.begin();
     } else {
       // software SPI
-      pinMode(_sck, OUTPUT);
-      pinMode(_mosi, OUTPUT);
-      pinMode(_miso, INPUT);
+      pinMode(_BME680_SoftwareSPI_SCK, OUTPUT);
+      pinMode(_BME680_SoftwareSPI_MOSI, OUTPUT);
+      pinMode(_BME680_SoftwareSPI_MISO, INPUT);
     }
+
+    gas_sensor.dev_id = _cs;
+    gas_sensor.intf = BME680_SPI_INTF;
+    gas_sensor.read = &spi_read;
+    gas_sensor.write = &spi_write;
   }
+
+  gas_sensor.delay_ms = delay;
+
+  int8_t rslt = BME680_OK;
+  rslt = bme680_init(&gas_sensor);
+  //Serial.print("Result: "); Serial.println(rslt);
+  if (rslt != BME680_OK) 
+    return false;
 
   setTemperatureOversampling(BME680_OS_8X);
   setHumidityOversampling(BME680_OS_2X);
@@ -239,27 +256,27 @@ bool Adafruit_BME680::setIIRFilterSize(uint8_t fs) {
 */
 /**************************************************************************/
 int8_t i2c_read(uint8_t dev_id, uint8_t reg_addr, uint8_t *reg_data, uint16_t len) {
-#ifdef DEBUG_I2C
-  Serial.print("\t$"); Serial.print(reg_addr, HEX); Serial.print(" => ");
+#ifdef BME680_DEBUG
+  Serial.print("\tI2C $"); Serial.print(reg_addr, HEX); Serial.print(" => ");
 #endif
 
   Wire.beginTransmission((uint8_t)dev_id);
   Wire.write((uint8_t)reg_addr);
   Wire.endTransmission();
   if (len != Wire.requestFrom((uint8_t)dev_id, (byte)len)) {
-#ifdef DEBUG_I2C
+#ifdef BME680_DEBUG
     Serial.print("Failed to read "); Serial.print(len); Serial.print(" bytes from "); Serial.println(dev_id, HEX);
 #endif
     return 1;
   }
   while (len--) {
     *reg_data = (uint8_t)Wire.read();
-#ifdef DEBUG_I2C
+#ifdef BME680_DEBUG
     Serial.print("0x"); Serial.print(*reg_data, HEX); Serial.print(", ");
 #endif
     reg_data++;
   }
-#ifdef DEBUG_I2C
+#ifdef BME680_DEBUG
   Serial.println("");
 #endif
   return 0;
@@ -271,20 +288,20 @@ int8_t i2c_read(uint8_t dev_id, uint8_t reg_addr, uint8_t *reg_data, uint16_t le
 */
 /**************************************************************************/
 int8_t i2c_write(uint8_t dev_id, uint8_t reg_addr, uint8_t *reg_data, uint16_t len) {
-#ifdef DEBUG_I2C
-  Serial.print("\t$"); Serial.print(reg_addr, HEX); Serial.print(" <= ");
+#ifdef BME680_DEBUG
+  Serial.print("\tI2C $"); Serial.print(reg_addr, HEX); Serial.print(" <= ");
 #endif
   Wire.beginTransmission((uint8_t)dev_id);
   Wire.write((uint8_t)reg_addr);
   while (len--) {
     Wire.write(*reg_data);
-#ifdef DEBUG_I2C
+#ifdef BME680_DEBUG
     Serial.print("0x"); Serial.print(*reg_data, HEX); Serial.print(", ");
 #endif
     reg_data++;
   }
   Wire.endTransmission();
-#ifdef DEBUG_I2C
+#ifdef BME680_DEBUG
   Serial.println("");
 #endif
   return 0;
@@ -292,8 +309,86 @@ int8_t i2c_write(uint8_t dev_id, uint8_t reg_addr, uint8_t *reg_data, uint16_t l
 
 
 
-uint8_t Adafruit_BME680::spixfer(uint8_t x) {
-  if (_sck == -1)
+/**************************************************************************/
+/*!
+    @brief  Reads 8 bit values over SPI
+*/
+/**************************************************************************/
+static int8_t spi_read(uint8_t cspin, uint8_t reg_addr, uint8_t *reg_data, uint16_t len) {
+#ifdef BME680_DEBUG
+  Serial.print("\tSPI $"); Serial.print(reg_addr, HEX); Serial.print(" => ");
+#endif
+
+  digitalWrite(cspin, LOW);
+
+  // If hardware SPI we should use transactions!
+  if (_BME680_SoftwareSPI_SCK == -1) {
+    SPI.beginTransaction(SPISettings(BME680_DEFAULT_SPIFREQ, MSBFIRST, SPI_MODE0));
+  }
+
+  spi_transfer(reg_addr);
+
+  while (len--) {
+    *reg_data = spi_transfer(0x00);
+#ifdef BME680_DEBUG
+    Serial.print("0x"); Serial.print(*reg_data, HEX); Serial.print(", ");
+#endif
+    reg_data++;
+  }
+
+  if (_BME680_SoftwareSPI_SCK == -1) {
+    SPI.endTransaction();
+  }
+
+  digitalWrite(cspin, HIGH);
+
+#ifdef BME680_DEBUG
+  Serial.println("");
+#endif
+  return 0;
+}
+
+/**************************************************************************/
+/*!
+    @brief  Writes 8 bit values over SPI
+*/
+/**************************************************************************/
+static int8_t spi_write(uint8_t cspin, uint8_t reg_addr, uint8_t *reg_data, uint16_t len) {
+#ifdef BME680_DEBUG
+  Serial.print("\tSPI $"); Serial.print(reg_addr, HEX); Serial.print(" <= ");
+#endif
+
+  digitalWrite(cspin, LOW);
+
+  // If hardware SPI we should use transactions!
+  if (_BME680_SoftwareSPI_SCK == -1) {
+    SPI.beginTransaction(SPISettings(BME680_DEFAULT_SPIFREQ, MSBFIRST, SPI_MODE0));
+  }
+
+  spi_transfer(reg_addr);
+  while (len--) {
+    spi_transfer(*reg_data);
+#ifdef BME680_DEBUG
+    Serial.print("0x"); Serial.print(*reg_data, HEX); Serial.print(", ");
+#endif
+    reg_data++;
+  }
+
+  if (_BME680_SoftwareSPI_SCK == -1) {
+    SPI.endTransaction();
+  }
+
+  digitalWrite(cspin, HIGH);
+
+#ifdef BME680_DEBUG
+  Serial.println("");
+#endif
+  return 0;
+}
+
+
+static uint8_t spi_transfer(uint8_t x) {
+  if (_BME680_SoftwareSPI_SCK == -1)
     return SPI.transfer(x);
 
   // software spi
@@ -301,10 +396,10 @@ uint8_t Adafruit_BME680::spixfer(uint8_t x) {
   uint8_t reply = 0;
   for (int i=7; i>=0; i--) {
     reply <<= 1;
-    digitalWrite(_sck, LOW);
-    digitalWrite(_mosi, x & (1<<i));
-    digitalWrite(_sck, HIGH);
-    if (digitalRead(_miso))
+    digitalWrite(_BME680_SoftwareSPI_SCK, LOW);
+    digitalWrite(_BME680_SoftwareSPI_MOSI, x & (1<<i));
+    digitalWrite(_BME680_SoftwareSPI_SCK, HIGH);
+    if (digitalRead(_BME680_SoftwareSPI_MISO))
       reply |= 1;
   }
   return reply;
--- Adafruit_BME680.h
+++ Adafruit_BME680.h
@@ -31,72 +31,23 @@
 
 int8_t i2c_write(uint8_t dev_id, uint8_t reg_addr, uint8_t *reg_data, uint16_t len);
 int8_t i2c_read(uint8_t dev_id, uint8_t reg_addr, uint8_t *reg_data, uint16_t len);
-
-
-#define BME680_SIGN_BIT_MASK					(0x08)
+int8_t spi_read(uint8_t dev_id, uint8_t reg_addr, uint8_t *reg_data, uint16_t len);
+int8_t spi_write(uint8_t dev_id, uint8_t reg_addr, uint8_t *reg_data, uint16_t len);
+uint8_t spi_transfer(uint8_t x);
 
 /*=========================================================================
     I2C ADDRESS/BITS
     -----------------------------------------------------------------------*/
 #define BME680_DEFAULT_ADDRESS                (0x77)
 /*=========================================================================*/
+#define BME680_DEFAULT_SPIFREQ               (1000000)
 
-/*=========================================================================
-    REGISTERS
-    -----------------------------------------------------------------------*/
-    enum
-    {
-      BME680_REGISTER_DIG_T1              = 0xE9,
-      BME680_REGISTER_DIG_T2              = 0x8A,
-      BME680_REGISTER_DIG_T3              = 0x8C,
-      BME680_REGISTER_DIG_T4              = 0x8D,
-      BME680_REGISTER_DIG_T5              = 0x93,
 
-      BME680_REGISTER_DIG_P1              = 0x8E,
-      BME680_REGISTER_DIG_P2              = 0x90,
-      BME680_REGISTER_DIG_P3              = 0x92,
-      BME680_REGISTER_DIG_P4              = 0x94,
-      BME680_REGISTER_DIG_P5              = 0x96,
-      BME680_REGISTER_DIG_P6              = 0x99,
-      BME680_REGISTER_DIG_P7              = 0x98,
-      BME680_REGISTER_DIG_P8              = 0x9C,
-      BME680_REGISTER_DIG_P9              = 0x9E,
-      BME680_REGISTER_DIG_P10             = 0xA0,
-      BME680_REGISTER_DIG_P11             = 0x9A,
-      BME680_REGISTER_DIG_P12             = 0x9B,
-      BME680_REGISTER_DIG_P13             = 0xA1,
-
-      BME680_REGISTER_DIG_H1              = 0xE2,
-      BME680_REGISTER_DIG_H2_MSB          = 0xE1,
-      BME680_REGISTER_DIG_H2_LSB          = 0xE2,
-      BME680_REGISTER_DIG_H3              = 0xE4,
-      BME680_REGISTER_DIG_H4              = 0xE5,
-      BME680_REGISTER_DIG_H5              = 0xE6,
-      BME680_REGISTER_DIG_H6              = 0xE7,
-      BME680_REGISTER_DIG_H7              = 0xE8,
-
-      BME680_REGISTER_DIG_GH1             = 0xED,
-      BME680_REGISTER_DIG_GH2             = 0xEB,
-      BME680_REGISTER_DIG_GH3             = 0xEE,
-
-      BME680_REGISTER_CHIPID             = 0xD0,
-
-      BME680_REGISTER_CONFIG       = 0x75,
-
-      BME680_REGISTER_PRESSUREDATA       = 0xF7,
-      BME680_REGISTER_TEMPDATA           = 0xFA,
-      BME680_REGISTER_HUMIDDATA          = 0xFD,
-      BME680_REGISTER_FIELD0_GASDATA          = 0x2A,
-      BME680_REGISTER_FIELD0_GASRANGE          = 0x2B,
-    };
-
-#define BME680_FILTER_COEFF_OFF 0
-/*=========================================================================*/
 
 /*
 class Adafruit_BME680_Unified : public Adafruit_Sensor
 {
-  public:
+public:
     Adafruit_BME680_Unified(int32_t sensorID = -1);
 
     bool  begin(uint8_t addr = BME680_ADDRESS);
@@ -113,7 +64,6 @@ class Adafruit_BME680_Unified : public Adafruit_Sensor
 };
 
 */
-
 
 class Adafruit_BME680
 {
@@ -139,10 +89,10 @@ class Adafruit_BME680
 
   private:
 
-    bool      _filterEnabled, _tempEnabled, _humEnabled, _presEnabled, _gasEnabled;
-    uint8_t   _i2caddr;
-    int32_t   _sensorID;
-    int8_t _cs, _mosi, _miso, _sck;
+    bool _filterEnabled, _tempEnabled, _humEnabled, _presEnabled, _gasEnabled;
+    uint8_t _i2caddr;
+    int32_t _sensorID;
+    int8_t _cs;
 
     uint8_t spixfer(uint8_t x);
 
