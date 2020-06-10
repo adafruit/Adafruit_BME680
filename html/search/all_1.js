05c026c1ba49c0f2a9938dfe8fc1b31a84cffab3
Doxygen CI <travis@travis-ci.org>
2020-06-10 18:29:47 +0000

    Deploy code docs to GitHub Pages Travis build:
    Commit:

--- html/search/all_1.js
+++ html/search/all_1.js
@@ -3,5 +3,5 @@ var searchData=
   ['adafruit_5fbme680',['Adafruit_BME680',['../class_adafruit___b_m_e680.html',1,'Adafruit_BME680'],['../class_adafruit___b_m_e680.html#ab61b5e1bc72e9fab8a2e63e86326eb9a',1,'Adafruit_BME680::Adafruit_BME680(TwoWire *theWire=&amp;Wire)'],['../class_adafruit___b_m_e680.html#ab0251724aa30376a65956d783f5a5a39',1,'Adafruit_BME680::Adafruit_BME680(int8_t cspin, SPIClass *theSPI=&amp;SPI)'],['../class_adafruit___b_m_e680.html#a8e89aaed66497bb2dde7ab678aa81173',1,'Adafruit_BME680::Adafruit_BME680(int8_t cspin, int8_t mosipin, int8_t misopin, int8_t sckpin)']]],
   ['adafruit_5fbme680_2ecpp',['Adafruit_BME680.cpp',['../_adafruit___b_m_e680_8cpp.html',1,'']]],
   ['adafruit_5fbme680_2eh',['Adafruit_BME680.h',['../_adafruit___b_m_e680_8h.html',1,'']]],
-  ['adafruit_20bme680_20temperature_2c_20humidity_2c_20barometric_20pressure_20and_20gas_20sensor_20driver',['Adafruit BME680 temperature, humidity, barometric pressure and gas sensor driver',['../index.html',1,'']]]
+  ['adafruit_20bme680_20temperature_2c_20humidity_2c_20barometric_20pressure_20and_20gas',['Adafruit BME680 temperature, humidity, barometric pressure and gas',['../index.html',1,'']]]
 ];
--- html/search/pages_0.js
+++ html/search/pages_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['adafruit_20bme680_20temperature_2c_20humidity_2c_20barometric_20pressure_20and_20gas_20sensor_20driver',['Adafruit BME680 temperature, humidity, barometric pressure and gas sensor driver',['../index.html',1,'']]]
+  ['adafruit_20bme680_20temperature_2c_20humidity_2c_20barometric_20pressure_20and_20gas',['Adafruit BME680 temperature, humidity, barometric pressure and gas',['../index.html',1,'']]]
 ];
